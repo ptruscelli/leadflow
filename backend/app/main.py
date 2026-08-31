e9d62cb934835245e2f8eda04d8437763856d35e
ptruscelli <137720740+ptruscelli@users.noreply.github.com>
2026-08-31 12:25:47 +0100

    Edit CORS to allow vercel + localhost frontend

--- backend/app/main.py
+++ backend/app/main.py
@@ -20,7 +20,10 @@ app = FastAPI(lifespan=lifespan)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[settings.frontend_domain],
+    allow_origins=[
+        settings.frontend_domain,
+        "http://localhost:3000",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
